44dc6683dff530589ce054ddfab356a4abfb949b
gowufang <wufang2009@foxmail.com>
2017-08-19 20:47:21 +0800

    继承BaseActivity

--- BroadcastForceOffLine/app/src/main/java/com/project/ics/broadcastforceoffline/MainActivity.java
+++ BroadcastForceOffLine/app/src/main/java/com/project/ics/broadcastforceoffline/MainActivity.java
@@ -2,11 +2,12 @@ package com.project.ics.broadcastforceoffline;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.project.ics.activities.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     Button forceofflinebtn;
     @Override
